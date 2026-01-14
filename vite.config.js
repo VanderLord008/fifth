8a71e9f7aede0a0f9762459ae1d3b45891d22f82
VanderLord008 <vaibhabtiwari9887@gmail.com>
2026-01-14 18:08:03 +0530

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/fifth/',  // GitHub Pages base path
+  base: '/',  // GitHub Pages base path
   build: {
     outDir: 'docs',  // Build to docs folder for GitHub Pages from main branch
   },
